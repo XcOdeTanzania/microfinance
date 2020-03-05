a51692197889b7fa4af3e12481fb679c10f4453f
mzanda <mijerck@gmail.com>
2020-03-05 19:21:01 +0300

    initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Microfinance Hawa
                 </div>
 
                 <div class="links">
