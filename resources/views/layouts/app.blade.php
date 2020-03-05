5a20964f188889da74041010d0923bb76911f00d
Henry Lyamuya <lyamuyahenry@gmail.com>
2020-03-05 21:01:29 +0300

    Protecting Sidebar and Top Nav-bar to authenticated user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,32 +2,37 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-    <meta name="description" content="Bootstrap Admin App" />
-    <meta name="keywords" content="app, responsive, jquery, bootstrap, dashboard, admin" />
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+    <meta name="description" content="Bootstrap Admin App"/>
+    <meta name="keywords" content="app, responsive, jquery, bootstrap, dashboard, admin"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
+    <link rel="icon" type="image/x-icon" href="{{asset('angle/favicon.ico')}}"/>
 
     <title>Angle - Bootstrap Admin Template</title>
 
     <!-- =============== VENDOR STYLES ===============-->
-    <link rel="stylesheet" href="{{ asset('angle/css/vendor.css') }}" />
+    <link rel="stylesheet" href="{{ asset('angle/css/vendor.css') }}"/>
     <!-- =============== BOOTSTRAP STYLES ===============-->
-    <link rel="stylesheet" href="{{ asset('angle/css/bootstrap.css') }}" data-rtl="{{ asset('angle/css/bootstrap-rtl.css') }}" id="bscss" />
+    <link rel="stylesheet" href="{{ asset('angle/css/bootstrap.css') }}"
+          data-rtl="{{ asset('angle/css/bootstrap-rtl.css') }}" id="bscss"/>
     <!-- =============== APP STYLES ===============-->
-    <link rel="stylesheet" href="{{ asset('angle/css/app.css') }}" data-rtl="{{ asset('angle/css/app-rtl.css') }}" id="maincss" />
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('angle/css/app.css') }}" data-rtl="{{ asset('angle/css/app-rtl.css') }}"
+          id="maincss"/>
     @yield('styles')
 
 </head>
 
 <body>
 <div class="wrapper">
+
+
+@if(Auth::user())
     <!-- top navbar-->
-@include('layouts.includes.header')
-<!-- sidebar-->
-@include('layouts.includes.sidebar')
+    @include('layouts.includes.header')
+    <!-- sidebar-->
+    @include('layouts.includes.sidebar')
+@endif
 <!-- offsidebar-->
 @include('layouts.includes.offsidebar')
 <!-- Main section-->
