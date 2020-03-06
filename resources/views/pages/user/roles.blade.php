81e4a52b18d3c58c1a3f556b84ccf1ee2119015e
hawally12 <hawaally0306@gmail.com>
2020-03-06 12:04:51 -0800

    user roles and use page completed

--- resources/views/pages/user/roles.blade.php
+++ resources/views/pages/user/roles.blade.php
@@ -1,67 +1,40 @@
 @extends('layouts.app')
 @section('content')
-    <div class="content-heading">
-        <div>
-            User Roles
-
-        </div>
-    </div>
-    <div class="container-fluid">
-        <!-- DATATABLE 1-->
-        <div class="card">
-            <div class="card-header">
-                <div class="card-title">User Roles</div>
-                
-            </div>
-            <div class="card-body">
+    <div class="content-wrapper">
+       <div class="content-heading">
+          <div>Roles</div>
+       </div>
+       <div class="container-fluid">
+          <!-- DATATABLE -->
+          <div class="card">
+             <div class="card-header"><br>
+        
+             <div class="card-body">
                 <table class="table table-striped my-4 w-100" id="datatable1">
-                    <thead>
-                        <tr>
-                            <th data-priority="1">Name</th>
-                            <th>Description</th>
-                            <th>Active</th>
-                            
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr class="User Roles">
-                            <td>Hawa</td>
-                            <td>Branch Manager</td>
-                            <td><i class="fa fa-check"></i></td>
-                        
-                        
-                            
-                            <td><button class="btn btn-labeled btn-info mb-2" type="button">
-                        <span class="btn-label"><i class="fa fa-eye"></i></span>
-                        view
-                    </button></td>
-                        </tr>
-
-                        <tr class="user">
-                            <td>Credit Officer</td>
-                            <td>Credit Officer</td>
-                            <td><i class="fa fa-check"></i></td>
-                            
-                            <td><button class="btn btn-labeled btn-info mb-2" type="button">
-                        <span class="btn-label"><i class="fa fa-eye"></i></span>
-                        view
-                    </button></td>
-                        </tr>    
-                        
-                        
-                       
-                        
-                        
-                        
-                       
-                    </tbody>
+                   <thead>
+                      <tr>
+                         <th>Name</th>
+                         <th>Description</th>
+                         <th>Active</th>
+                         <th>Status</th>
+                      </tr>
+                   </thead>
+                   <tbody> 
+                      <tr class="gradeC">
+                         <td>Hawa12</td>
+                         <td>Head Office</td>
+                         <td><i class="fa fa-check"></i></td>
+                         <td><button class="btn btn-primary"><i class="fa fa-eye">&nbsp;view</i></a></td>
+                      </tr>
+                      
+                   </tbody>
                 </table>
-            </div>
-        </div>
-       
-    </div>
-@endsection
-@section('styles')@endsection
-@section('scripts')
+             </div>
+         
+          </div>
+          
+    @endsection
+    @section('styles')@endsection
+    @section('scripts')
     <script src="{{ asset('angle/js/datatable.js') }}"></script>
-@endsection
+    @endsection
