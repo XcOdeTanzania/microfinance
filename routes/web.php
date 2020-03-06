1cce86ec17f9c1858e22aa781dee09b26fcd3fe7
Mhando <mhandomatty@gmail.com>
2020-03-06 10:24:32 +0300

    Accounting journals view completed

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,7 @@ Route::get('/client/register',function (){
 Route::get('/accounting/chartsOfAccounts',function (){
     return view('pages.accounting.chartsOfAccounts');
 });
+
+Route::get('/accounting/journals',function (){
+    return view('pages.accounting.journals');
+});
