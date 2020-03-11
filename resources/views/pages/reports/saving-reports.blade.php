59ccb0e5115a727a9de670b39bd0040f7c26afd7
NchyG <godsonnchy@gmail.com>
2020-03-11 12:36:04 +0300

    update savings report view

--- resources/views/pages/reports/saving-reports.blade.php
+++ resources/views/pages/reports/saving-reports.blade.php
@@ -1,80 +1,95 @@
-@extends('layouts.app');
-
-
-@section('styles')
-    
-@endsection
-
+@extends('layouts.app')
 
 @section('content')
     <div class="content-heading">
        <div>Savings Reports</div>
     </div>
+    
     <div class="container-fluid">
-       <!-- DATATABLE -->
-       <div class="card">
-          <div class="card-header"><br>
-          <div class="card-body">
-             <table class="table table-striped my-4 w-100" id="datatable1">
-                <thead>
-                   <tr>
-                      <th data-priority="1">Name</th>
-                      <th>Category</th>
-                      <th>Description</th>
-                      <th></th>
-                   </tr>
-                </thead>
-                <tbody> 
-                    <tr class="gradeC">
-                        <td>
-                            <a href="#">All Savings Transactions</a>
-                        </td>
-                        <td>Savings</td>
-                        <td>This report shows individual deposits and withdrawals from savings within the specified time period.</td>
-                        <td><button type="btn" class="btn btn-primary"><span class="fas fa-file"></span></button></td>
-                    </tr>
-                   <tr class="gradeC">
-                      <td>
-                          <a href="#">Fixed Term Deposit Accounts Overview</a>
-                      </td>
-                      <td>Savings</td>
-                      <td>This report shows individual deposits and withdrawals from savings within the specified time period.</td>
-                      <td><button type="btn" class="btn btn-primary"><span class="fas fa-file"></span></button></td>
-                   </tr>
-                   <tr class="gradeC">
+      <!-- DATATABLE DEMO 3-->
+      <div class="card">
+        <!--<div class="card-header">
+           <div class="card-title">Key Table</div>
+           <div class="text-sm">KeyTable allows you to use keyboard navigation on a DataTables enhanced table, like an Excel spreadsheet.</div>
+        </div> -->
+        <div class="card-body">
+           <table class="table table-striped my-4 w-100" id="datatable3">
+              <thead>
+                 <tr>
+                    <th>Name</th>
+                    <th>Category</th>
+                    <th>Description</th>
+                    <th></th>
+                 </tr>
+              </thead>
+              <tbody>
+                 <tr>
                     <td>
-                        <a href="#">Fixed Term Maturity Report</a>
+                       <a href="#">All Savings Transactions</a>
                     </td>
-                    <td>Savings</td>
-                    <td>This report shows individual deposits and withdrawals from savings within the specified time period.</td>
-                    <td><button type="btn" class="btn btn-primary"><span class="fas fa-file"></span></button></td>
-                   </tr>
-
-                   <tr class="gradeC">
                     <td>
-                        <a href="#">Savings Accounts Report</a>
+                       Savings
                     </td>
-                    <td>Savings</td>
-                    <td>This report shows individual deposits and withdrawals from savings within the specified time period.</td>
-                    <td><button type="btn" class="btn btn-primary"><span class="fas fa-file"></span></button></td>
-                   </tr>
-                   
-                   <tr class="gradeC">
                     <td>
-                        <a href="#">Savings Balances</a>
+                        This report shows individual deposits and withdrawals from savings within the specified time period.
                     </td>
-                    <td>Savings</td>
-                    <td>This report shows individual deposits and withdrawals from savings within the specified time period.</td>
-                    <td><button type="btn" class="btn btn-primary"><span class="fas fa-file"></span></button></td>
-                   </tr>
-                </tbody>
-             </table>
-          </div>
-      
-       </div>
-       </div>
-    </div>
-
+                    <td>
+                       <a href="#" class="btn btn-primary">
+                          <i class="fas fa-file"></i>
+                       </a>
+                    </td>
+                 </tr>
+                 <tr>
+                    <td>
+                       <a href="#">Fixed Term Deposit Accounts Overview/a>
+                    </td>
+                    <td>
+                       Savings
+                    </td>
+                    <td>
+                        This report shows individual deposits and withdrawals from savings within the specified time period.
+                    </td>
+                    <td>
+                       <a href="#" class="btn btn-primary">
+                          <i class="fas fa-file"></i>
+                       </a>
+                    </td>
+                 </tr><tr>
+                    <td>
+                       <a href="#">Fixed Term Maturity Report</a>
+                    </td>
+                    <td>
+                       Savings
+                    </td>
+                    <td>
+                        This report shows individual deposits and withdrawals from savings within the specified time period.
+                    </td>
+                    <td>
+                       <a href="#" class="btn btn-primary">
+                          <i class="fas fa-file"></i>
+                       </a>
+                    </td>
+                 </tr><tr>
+                    <td>
+                       <a href="#">Savings Accounts Report</a>
+                    </td>
+                    <td>
+                       Savings
+                    </td>
+                    <td>
+                        This report shows individual deposits and withdrawals from savings within the specified time period.
+                    </td>
+                    <td>
+                       <a href="#" class="btn btn-primary">
+                          <i class="fas fa-file"></i>
+                       </a>
+                    </td>
+                 </tr>
+              </tbody>
+           </table>
+        </div>
+     </div>
+   </div>
 @endsection
 
 @section('scripts')
