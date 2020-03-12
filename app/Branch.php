35a47b0a8c9a1bfebca0f3e3daf1e418f751828f
NchyG <godsonnchy@gmail.com>
2020-03-12 15:45:05 +0300

    branch migration, model and controller complete

---
+++ app/Branch.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Branch extends Model
+{
+
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'category',
+        'description'
+    ];
+    
+}
