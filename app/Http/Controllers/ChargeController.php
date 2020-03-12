f1ea6f09d902a76273676e4d1295240f4955e51d
dela728 <sumace01@gmail.com>
2020-03-12 16:15:17 +0300

    controllers for charge,kin and guarantor updated

--- app/Http/Controllers/ChargeController.php
+++ app/Http/Controllers/ChargeController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Guarantor;
+use App\Charge;
 use Illuminate\Http\Request;
 
-class GuarantorsController extends Controller
+class ChargeController extends Controller
 {
     //controller functions
 }
