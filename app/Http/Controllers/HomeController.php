63bab7518fbae31c77df37bc40bd02657dd88420
hawally12 <hawaally0306@gmail.com>
2020-03-12 03:26:12 -0700

    creating controllet and models

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,8 +21,13 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function homePage()
     {
         return view('home');
+    }
+
+    public function registerClient()
+    {
+        return view('pages.client.register');
     }
 }
---
+++ database/migrations/2020_03_12_091820_create_profiles_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
+            $table->string('gender');
+            $table->string('phone_number_one');
+            $table->string('phone_number_two')->nullable();
+            $table->date('date_of_birth');
+            $table->string('tags');
+            $table->string('town');
+            $table->string('postal_address');
+            $table->string('marital_status');
+            $table->integer('district_id');
+            $table->double('latitude')->nullable();
+            $table->double('longitude')->nullable();
+            $table->string('email')->nullable();
+            $table->bigInteger('user_id');
+
+            $table->softdeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
