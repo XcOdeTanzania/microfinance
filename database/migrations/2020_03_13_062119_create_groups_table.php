38bdcd5717c8848b3dd5795fe9022cd9b38fbfb2
Henry Lyamuya <lyamuyahenry@gmail.com>
2020-03-13 09:45:56 +0300

    Groups migration completed

---
+++ database/migrations/2020_03_13_062119_create_groups_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->integer('branch_id');
+            $table->string('account_number');
+            $table->string('uid');
+            $table->integer('status_id');
+            $table->date('activation_date');
+            $table->integer('meeting_day');
+            $table->string('meeting_location');
+            $table->string('meeting_frequency');
+
+            $table->softdeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
