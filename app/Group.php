1763ce9775ca6baeb44030893d995195eef67a55
Henry Lyamuya <lyamuyahenry@gmail.com>
2020-03-13 09:31:03 +0300

    Group Model completed

---
+++ app/Group.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class group extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'branch_id',
+        'account_number',
+        'uid',
+        'status_id',
+        'activation_date',
+        'meeting_day',
+        'meeting_location',
+        'meeting_frequency'
+    ];
+
+
+    protected $dates = [
+        'deleted_at'
+    ];
+}
