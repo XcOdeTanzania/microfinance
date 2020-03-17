8911afd42a7a43784d7bb3ff7670aec713d9402d
admin <kalrobbynson@gmail.com>
2020-03-17 11:13:56 +0300

    report table seeder added

--- database/migrations/2020_03_12_111824_create_reports_table.php
+++ database/migrations/2020_03_12_111824_create_reports_table.php
@@ -17,7 +17,6 @@ class CreateReportsTable extends Migration
             $table->id();
 
             $table->string('name');
-            $table->string('category');
             $table->string('description');
             $table->integer('reportable_id');
             $table->string('reportable_type');
