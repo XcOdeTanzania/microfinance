df5c4c741c9d142379846f3e1d8bf97b20ef9b24
Henry Lyamuya <lyamuyahenry@gmail.com>
2020-03-17 19:37:46 +0300

    dynamic adding table rows to field

--- resources/views/pages/loan/create.blade.php
+++ resources/views/pages/loan/create.blade.php
@@ -204,7 +204,7 @@
                                     <th>Payment Mode</th>
                                 </tr>
                                 </thead>
-                                <tbody>
+                                <tbody id="chargesTableBody">
                                 <tr>
                                     <td>Loan Account fee</td>
                                     <td>Flat</td>
@@ -219,14 +219,14 @@
                         <div class="form-row align-items-center">
                             <div class="col-auto">
                                 <label class="sr-only" for="charge">Charge</label>
-                                <select class="form-control mb-2" id="charge" name="charge">
-                                    <option value="late">Late payment fee</option>
-                                    <option selected value="">Insufficient fund</option>
-                                    <option value="">Application fee</option>
+                                <select id="selectCharge" class="form-control mb-2"  name="charge" >
+                                    @foreach($charges as $charge)
+                                    <option value="{{$charge}}" >{{$charge->name}}</option>
+                                    @endforeach
                                 </select>
                             </div>
                             <div class="col-auto">
-                                <button class="btn btn-primary mb-2" type="button">Add To Product</button>
+                                <button class="btn btn-primary mb-2" onclick="addChargesToTable();" type="button">Add To Product</button>
                             </div>
                         </div>
                     </fieldset>
@@ -249,17 +249,7 @@
                                     <th>Remove</th>
                                 </tr>
                                 </thead>
-                                <tbody>
-                                <tr>
-                                    <td>Fixed</td>
-                                    <td>20,0000,000</td>
-                                    <td>House</td>
-                                    <td>
-                                        <em class="icon-close">
-
-                                        </em>
-                                    </td>
-                                </tr>
+                                <tbody id="collateralTableBody">
                                 </tbody>
                             </table>
                         </div>
@@ -279,21 +269,11 @@
                                 <tr class="bg-gray">
                                     <th>Name</th>
                                     <th>Guarantor type</th>
-                                    <th>Guarantee amount</th>
+                                    <th>Relationship</th>
                                     <th>Remove</th>
                                 </tr>
                                 </thead>
-                                <tbody>
-                                <tr>
-                                    <td>John Doe</td>
-                                    <td>Existing client</td>
-                                    <td></td>
-                                    <td>
-                                        <em class="icon-close">
-
-                                        </em>
-                                    </td>
-                                </tr>
+                                <tbody id="existingClientTable">
                                 </tbody>
                             </table>
                         </div>
@@ -308,7 +288,7 @@
                     <h4>
                         Summary
                         <br/>
-                        <small> Verify Loan information </small>
+                        <small> Verify Loan information</small>
                     </h4>
                     <fieldset class="overflow-auto">
                         <button class="btn btn-sm btn-primary" type="submit">Submit Application</button>
@@ -322,6 +302,119 @@
 @section('scripts')
     <script src="{{ asset('angle/js/wizard.js') }}"></script>
     <script src="{{ asset('angle/js/forms.js') }}"></script>
+    <script>
+
+
+        $('#addGuarantorModal').on('hidden.bs.modal', function (e) {
+            console.log('Guarantor closed');
+        });
+        var clientsList = [];
+        $('#addGuarantorModal').on('shown.bs.modal', function (e) {
+            console.log('Guarantor Opened');
+
+
+        });
+// fetch all clients from request
+        var result =  fetch('{{route('client.list')}}');
+        result.then(function (result) {
+            return result.json();
+        }).then(function (clients) {
+
+            clients['clients'].forEach(function (client){
+                clientsList.push(client['id']+' '+client['profile']['first_name']+' '+client['profile']['middle_name']+' '+client['profile']['last_name'])
+            });
+
+        }).catch (function (error) {
+            console.log(error);
+            return {'error':error};
+        });
+
+        // End fetching clients
+
+        //load data to auto complete function
+        autoComplete(document.getElementById("existingClientName"), clientsList);
+
+        // Append guarantor row in a table body
+        function  addGuarantorToTable( name,type,relationship) {
+            var row =  '<tr>'  +
+                ' <td> <input type="text" value="'+name+'" name="guarantor_name" readonly  hidden> '+name+' </td>'+
+                '<td> <input type="text" value="'+type+'" name="guarantor_type" readonly hidden> '+type+' </td>'+
+                '<td>  <input type="text" value="'+relationship+'" name="guarantor_relationship" readonly hidden> '+relationship+' </td>'+
+                '<td> <em class="icon-close" onclick="removeTableRow(this);"> </em> </td>'+
+                '</tr>';
+
+            $('#existingClientTable').append(row);
+        }
+// end add guarantor table row
+
+        // populates guarantor info to a table
+        function addExistingGuarantorToTable () {
+            var name = $('#existingClientName').val();
+            var relationship = $('#existingClientRelationship').val();
+            addGuarantorToTable(name,'Existing Client',relationship);
+        }
+        // end populating guarantor to the table
+
+        // remove the row from the table
+        function  removeTableRow(element) {
+            $(element).parents("tr").remove()
+            //element.parents("tr").remove();
+
+        }
+
+
+        // Collateral
+
+        // Append collateral row in a table body
+        function  addCollateralToTable( type,value,description) {
+            var row =  '<tr>'  +
+                ' <td> <input type="text" value="'+type+'" name="collateral_type" readonly  hidden> '+type+' </td>'+
+                '<td> <input type="text" value="'+value+'" name="collateral_value" readonly hidden> '+value+' </td>'+
+                '<td>  <input type="text" value="'+description+'" name="collateral_description" readonly hidden> '+description+' </td>'+
+                '<td> <em class="icon-close" onclick="removeTableRow(this);"> </em> </td>'+
+                '</tr>';
+
+            $('#collateralTableBody').append(row);
+        }
+        // end add guarantor table row
+
+        function addCollateralModalButton() {
+            var type=$('#collateralModalType').val();
+            var value=$('#collateralModalValue').val();
+            var description = $('#collateralModalDescription').val();
+
+            addCollateralToTable(type,value,description);
+
+        }
+
+        // End of Collateral
+
+        // Charges
+
+        function addChargesToTable() {
+            var charge = $('#selectCharge').val();
+
+            var obj = JSON.parse(charge);
+            console.log(obj.amount);
+            var row = ''+
+                    '<input type="hidden" name="charge_id" value="'+obj.id+'">'+
+                '<tr>'+
+                '<td >'+obj.name+'</td>'+
+            '<td>'+obj.type+'</td>'+
+            '<td>'+obj.amount+'</td>'+
+            '<td>'+obj.collected_on+'</td>'+
+            '<td>'+obj.date+'</td>'+
+            '<td>'+obj.payment_mode+'</td>'+
+            '</tr>';
+            $('#chargesTableBody').append(row);
+        }
+
+        // End charges
+
+
+
+
+    </script>
 @endsection
 <div class="modal fade" id="addCollateralModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
      aria-hidden="true">
@@ -333,34 +426,32 @@
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <form action="">
                 <div class="modal-body">
 
                     <div class="form-group">
-                        <label for="loanType"> Collateral Type </label>
-                        <select class="custom-select " name="loanType" id="loanType">
+                        <label for="collateralModalType"> Collateral Type </label>
+                        <select class="custom-select" name="collateralType" id="collateralModalType">
                             <option selected value="no"></option>
-                            <option value="">Fixed Asset</option>
-                            <option value="">Motor Vehicle</option>
-                            <option value="">Electrical Appliance</option>
+                            <option value="Fixed Asset">Fixed Asset</option>
+                            <option value="Motor Vehicle">Motor Vehicle</option>
+                            <option value="Electrical Appliance">Electrical Appliance</option>
                         </select>
                     </div>
                     <div class="form-group">
-                        <label for="collateralValue">Value</label>
-                        <input class="form-control" type="number" id="collateralValue" name="collateralValue">
+                        <label for="collateralModalValue">Value</label>
+                        <input class="form-control" type="number" id="collateralModalValue" name="collateralValue">
                     </div>
                     <div class="form-group">
-                        <label for="description">Description</label>
-                        <input class="form-control" type="text" id="description" name="description">
+                        <label for="collateralModalDescription">Description</label>
+                        <input class="form-control" type="text" id="collateralModalDescription" name="description">
                     </div>
 
 
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Add</button>
+                    <button type="button" onclick="addCollateralModalButton();" class="btn btn-primary">Add</button>
                 </div>
-            </form>
         </div>
     </div>
 </div>
@@ -378,41 +469,51 @@
             <div class="modal-body">
                 <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
                     <li class="nav-item">
-                        <a class="nav-link active" id="pills-existing-client-tab" data-toggle="pill" href="#pills-existingClient" role="tab"
+                        <a class="nav-link active" id="pills-existing-client-tab" data-toggle="pill"
+                           href="#pills-existingClient" role="tab"
                            aria-controls="pills-existingClient" aria-selected="true">Existing Client</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" id="pills-non-client-tab" data-toggle="pill" href="#pills-non-client" role="tab"
+                        <a class="nav-link" id="pills-non-client-tab" data-toggle="pill" href="#pills-non-client"
+                           role="tab"
                            aria-controls="pills-non-client" aria-selected="false">Non Client</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" id="pills-group-member-tab" data-toggle="pill" href="#pills-group-member" role="tab"
+                        <a class="nav-link" id="pills-group-member-tab" data-toggle="pill" href="#pills-group-member"
+                           role="tab"
                            aria-controls="pills-group-member" aria-selected="false">Group Member</a>
                     </li>
                 </ul>
                 <div class="tab-content" id="pills-tabContent">
-                    <div class="tab-pane fade show active" id="pills-existingClient" role="tabpanel" aria-labelledby="pills-existing-client-tab">
-                        <form action="">
+                    <div class="tab-pane fade show active" id="pills-existingClient" role="tabpanel"
+                         aria-labelledby="pills-existing-client-tab">
+
+                        <div class="form-group">
                             <div class="form-group">
-                                <div class="form-group">
-                                    <label for="client">Client</label>
-                                    <input class="form-control" type="text" id="client" name="client">
+                                <label for="existingClientName">Client</label>
+                                <div class="autocomplete">
+                                    <input id="existingClientName" class="form-control" type="text" name="client">
                                 </div>
-                                <label for="relationship"> Relationship </label>
-                                <select class="custom-select " name="relationship" id="relationship">
-                                    <option selected value=""></option>
-                                    <option value="">Parent</option>
-                                    <option value="">Spouse</option>
-                                    <option value="">Sibling</option>
-                                    <option value="">Business Partner</option>
-                                    <option value="">Friend</option>
-                                </select>
+
                             </div>
-                            <button class="btn btn-sm btn-primary" type="submit">Add</button>
-                        </form>
+                            <label for="existingClientRelationship"> Relationship </label>
+                            <select class="custom-select " name="relationship" id="existingClientRelationship">
+                                <option selected value=""></option>
+                                <option value="Parent">Parent</option>
+                                <option value="Spouse">Spouse</option>
+                                <option value="Sibling">Sibling</option>
+                                <option value="Business Partner">Business Partner</option>
+                                <option value="Friend">Friend</option>
+                            </select>
+                        </div>
+                        <button id="addGuarantorModalButton" class="btn btn-sm btn-primary"
+                                onclick="addExistingGuarantorToTable();" type="button">Add
+                        </button>
+
                     </div>
-                    <div class="tab-pane fade" id="pills-non-client" role="tabpanel" aria-labelledby="pills-non-client-tab">
-                        <form action="">
+                    <div class="tab-pane fade" id="pills-non-client" role="tabpanel"
+                         aria-labelledby="pills-non-client-tab">
+
                             <div class="form-group">
                                 <label for="relationship"> Relationship </label>
                                 <select class="custom-select " name="relationship" id="relationship">
@@ -465,15 +566,18 @@
                                 <input class="form-control" type="text" id="comment" name="comment">
                             </div>
                             <button class="btn btn-sm btn-primary" type="submit">Add</button>
-                        </form>
+
                     </div>
-                    <div class="tab-pane fade" id="pills-group-member" role="tabpanel" aria-labelledby="pills-group-member-tab">
+                    <div class="tab-pane fade" id="pills-group-member" role="tabpanel"
+                         aria-labelledby="pills-group-member-tab">
                         Group Member
                     </div>
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button"  class="btn btn-secondary" data-dismiss="modal">
+                    Close
+                </button>
             </div>
 
         </div>
