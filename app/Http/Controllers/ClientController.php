c98ade8de624144f87d152a5e65aafc91656413a
Henry Lyamuya <lyamuyahenry@gmail.com>
2020-03-17 19:31:00 +0300

    adding get client list function

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,67 +2,85 @@
 
 namespace App\Http\Controllers;
 
+use App\Client;
 use App\Clients;
 use App\District;
 use App\Region;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
-{ 
+{
 
-   // Views Functions
+    // Views Functions
 
-        /**
-        * Display pendingApproval.
-        *
-        * @return pendingApprovalPage view in client
-        */
-       public function pendingApprovalPage()
-       {
+    /**
+     * Display pendingApproval.
+     *
+     * @return pendingApprovalPage view in client
+     */
+    public function pendingApprovalPage()
+    {
+        $clients = Client::all();
+        return view('pages.client.pendingApproval', ['clients' => $clients]);
+    }
 
-          return view('pages.client.pendingApproval');
-}
-        /**
-        * Display closed.
-        *
-        * @return closedPage view in client
-        */
-       public function closedPage()
-        {
-           return view('pages.client.closed');
-}
-        /**
-        * Display rejected.
-        *
-        * @return rejectedPage view in client
-        */
-        public function rejectedPage()
-        {
-           return view('pages.client.rejected');
-}
-        /**
-        * Display transfer.
-        *
-        * @return transferPage view in client
-        */
-        public function transferPage()
-        {
-           return view('pages.client.transfer'); 
-}
-       /**
-        * Display registerClient.
-        *
-        * @return transferPage view in client
-        */
-        public function registerClientPage()
-        {
-           $regions = Region::all();
-           $districts = District::all();
-           return view('pages.client.register',['regions'=>$regions,'districts'=>$districts]); 
-}
+    /**
+     * Display closed.
+     *
+     * @return closedPage view in client
+     */
+    public function closedPage()
+    {
+        $clients = Client::class;
+        return view('pages.client.closed', ['clients' => $clients]);
+    }
+
+    /**
+     * Display rejected.
+     *
+     * @return rejectedPage view in client
+     */
+    public function rejectedPage()
+    {
+        $clients = Client::class;
+        return view('pages.client.rejected', ['clients' => $clients]);
+    }
+
+    /**
+     * Display transfer.
+     *
+     * @return transferPage view in client
+     */
+    public function transferPage()
+    {
+        $clients = Client::class;
+        return view('pages.client.transfer', ['clients' => $clients]);
+    }
+
+    /**
+     * Display registerClient.
+     *
+     * @return transferPage view in client
+     */
+    public function registerClientPage()
+    {
+        $regions = Region::all();
+        $districts = District::all();
+        return view('pages.client.register', ['regions' => $regions, 'districts' => $districts]);
+    }
+
+    public function getClientsList()
+    {
+        $clients = Client::all();
+        foreach ($clients as $client){
+            $client->profile = $client->user->profile;
+        }
+        return response()->json([
+            'clients' => $clients
+        ], 200, [], JSON_NUMERIC_CHECK);
+    }
 
 // Logical Functions
 
- 
 
 }
