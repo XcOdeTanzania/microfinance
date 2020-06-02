2b55dea612f089c3b5b2262d539ad952b45fc3b6
Henry Lyamuya <lyamuyahenry@gmail.com>
2020-06-02 18:19:15 +0300

    Merge branch 'master' of https://github.com/XcOdeTanzania/microfinance
    # Conflicts:
    #	routes/web.php

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -8,235 +8,50 @@ use App\Group;
 use App\Loan;
 use App\LoanStatus;
 use App\LoanType;
+use App\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class LoanController extends Controller
 {
-    /**
-     * Display a create loans page.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function createLoanPage()
+
+
+    public function getLoans($status)
     {
-        $charges = Charge::all();
-        $groups = Group::all();
-        $loanableNames = array();
-        $clients = Client::all();
-
-        foreach ($clients as $client) {
-            array_push($loanableNames, $client->id . ' ' . $client->user->profile->first_name . ' ' . $client->user->profile->middle_name . ' ' . $client->user->profile->last_name);
-            //array_push($loanableNames,$client->id.' '.$client->user->profile->first_name.' '.$client->user->profile->middle_name.' '.$client->user->profile->last_name);
-        }
-        foreach ($groups as $group) {
-            array_push($loanableNames, $group->id . ' ' . $group->name);
-        }
-
-        return view('pages.loan.create', ['charges' => $charges, 'loanableNames' => $loanableNames]);
-    }
-
-    /**
-     * Display  all loans.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function loansPage()
-    {
-        return view('pages.loan.view');
-    }
-
-    /**
-     * Display  loan details.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function loanDetailsPage($id)
-    {
-        $loan = Loan::find($id);
-
-
-        if (!$loan) return back()->with("error", "Loan not found");
-
-        /////
-
-        $val = substr($loan->loanable_type, 4);
-        $val_to_lower = strtolower($val);
-        $loan->$val_to_lower = $loan->loanable_type::find($loan->loanable_id);
-        $loan->officer = $loan->loanable_type::find($loan->loanable_id)->user;
-        $loan->branch = $loan->loanable_type::find($loan->loanable_id)->branch;
-        $loan->summary;
-        $loan->summaryPrincipal;
-        $loan->summaryInterest;
-        $loan->summaryFee;
-        $loan->summaryPenalty;
-        $loan->repayments;
-        $loan->transactions;
-        $loan->guarantors;
-        $loan->collaterals;
-        $loan->standingInstructions;
-        $loan->audits;
-        $loan->surveys;
-        
-
-        $loan->product = LoanType::find($loan->loan_type_id);
-        $loan->status = LoanStatus::find($loan->loan_status_id);
-        if ($val_to_lower == 'group') {
-            $group = $loan->loanable_type::find($loan->loanable_id);
-            $loan->clients = $group->clients;
-            $loan->groupMemberAllocation;
-        }
-
-
-        $loan->laonable;
-
-        /////
-
-
-
-        // dd(@json_encode($loan));
-        // return response()->json(['loan'=> $loan]);
-        return view('pages.loan.details', ['loan' => $loan]);
-    }
-
-    /**
-     * Display  loans pending approval.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function loanPendingApprovalPage()
-    {
-        return view('pages.loan.pending-approval');
-    }
-
-    /**
-     * Display  loans pending second approval.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function loanPendingSecondApprovalPage()
-    {
-        return view('pages.loan.pendingSecondApproval');
-    }
-
-    /**
-     * Display  pending loan details.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function loanPendingDetailsPage($id)
-    {
-        return view('pages.loan.pending', ['id' => $id]);
-    }
-
-    /**
-     * Display  pending loan details.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function loanCalculatorPage()
-    {
-        return view('pages.loan.calculator');
-    }
-
-    public function overpaidPage()
-    {
-        return view('pages.loan.overpaid');
-    }
-
-    public function awaitingPage()
-    {
-        return view('pages.loan.awaiting');
-    }
-
-    public function rejectedPage()
-    {
-        return view('pages.loan.rejected');
-    }
-
-    public function writtenOffPage()
-    {
-        return view('pages.loan.written-off');
-    }
-
-    public function closedPage()
-    {
-        return view('pages.loan.closed');
-    }
-
-    public function withdrawPage()
-    {
-        return view('pages.loan.withdraw');
-    }
-
-
-
-    public function postLoan(Request $request)
-    {
-
-
-        return response()->json([
-            'request' => $request->all()
-        ], 200, [], JSON_NUMERIC_CHECK);
-
-        //        return back()->with('message',$request);
-    }
-
-    /**
-     * Function to call all loans
-     * @param Request $request 
-     */
-    public function getAllLoans(Request $request)
-    {
-        if ($request->status == null) {
-            $loans = Loan::all();
-        } else {
-            $filteredloans = Loan::all()->map(function ($loan) {
+        $loans = Loan::all();
+        if ($status != 'all') {
+            $$loans = $loans->map(function ($loan) {
                 return $loan;
             })
-                ->reject(function ($loan) use ($request) {
-                    $status = LoanStatus::find($loan->loan_status_id);
-
-
-                    return $status->name != $request->status;
-                });
-
-
-            foreach ($filteredloans  as $key => $value) {
-                $loans[] = $value;
-            }
+                ->reject(function ($loan) use ($status) {
+                    return $loan->status != $status;
+                })->values();
         }
-
-
-
 
         foreach ($loans as $loan) {
 
-            $val = substr($loan->loanable_type, 4);
-            $val_to_lower = strtolower($val);
-            $loan->$val_to_lower = $loan->loanable_type::find($loan->loanable_id);
-            $loan->officer = $loan->loanable_type::find($loan->loanable_id)->user;
-            $loan->branch = $loan->loanable_type::find($loan->loanable_id)->branch;
-            $loan->summary;
-            $loan->summaryPrincipal;
-            $loan->summaryInterest;
-            $loan->summaryFee;
-            $loan->summaryPenalty;
-            $loan->repayments;
-            $loan->transactions;
-            $loan->guarantors;
-            $loan->collaterals;
-            $loan->standingInstructions;
-            $loan->audits;
-            $loan->surveys;
-            
+            // $val = substr($loan->loanable_type, 4);
+            // $val_to_lower = strtolower($val);
+            // $loan->$val_to_lower = $loan->loanable_type::find($loan->loanable_id);
+            // $loan->officer = $loan->loanable_type::find($loan->loanable_id)->user;
+            // $loan->branch = $loan->loanable_type::find($loan->loanable_id)->branch;
+            // $loan->summary;
+            // $loan->summaryPrincipal;
+            // $loan->summaryInterest;
+            // $loan->summaryFee;
+            // $loan->summaryPenalty;
+            // $loan->repayments;
+            // $loan->transactions;
+            // $loan->guarantors;
+            // $loan->collaterals;
+            // $loan->standingInstructions;
+            // $loan->audits;
+            // $loan->surveys;
 
-            $loan->product = LoanType::find($loan->loan_type_id);
-            $loan->status = LoanStatus::find($loan->loan_status_id);
-            if ($val_to_lower == 'group') {
-                $group = $loan->loanable_type::find($loan->loanable_id);
-                $loan->clients = $group->clients;
-                $loan->groupMemberAllocation;
-            }
+
+            $loan->loan_type = LoanType::find($loan->loan_type_id);
+
 
 
             $loan->laonable;
@@ -244,5 +59,66 @@ class LoanController extends Controller
         return response()->json([
             'loans' => $loans,
         ], 200, [], JSON_NUMERIC_CHECK);
+    }
+
+
+    //get all Loan
+    public function getLoan($loanId)
+    {
+
+        $loan = Loan::find($loanId);
+        if (!$loan) return response()->json(['error' => 'Loan not found']);
+
+        return response()->json(['loan' => $loan], 200, [], JSON_NUMERIC_CHECK);
+    }
+
+    // put Loan
+    public function putLoan(Request $request, $businessId)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'status' => 'required',
+                'amount' => 'required',
+                'orign_of_funding' => 'required',
+                'duration' => 'required',
+                'repayment_every' => 'required',
+                'repayment_every_type' => 'required',
+                'purpose' => 'required',
+                'auto_create_standing_instruction' => 'required',
+                'sector' => 'required',
+                'channel' => 'required',
+            ]
+        );
+        if ($validator->fails())
+            return response()->json(['error', $validator->errors()]);
+
+        $loan = Loan::find($businessId);
+        if (!$loan) return response()->json(['error' => 'Loan not found']);
+
+        $loan->update([
+            'status' => $request->status,
+            'amount' => $request->amount,
+            'orign_of_funding' => $request->orign_of_funding,
+            'duration' => $request->duration,
+            'repayment_every' => $request->repayment_every,
+            'repayment_every_type' => $request->repayment_every_type,
+            'purpose' => $request->purpose,
+            'auto_create_standing_instruction' => $request->auto_create_standing_instruction,
+            'sector' => $request->sector,
+            'channel' => $request->channel
+        ]);
+
+        return response()->json(['loan' => $loan], 200, [], JSON_NUMERIC_CHECK);
+    }
+
+    //delete Loan
+    public function deleteLoan($loanId)
+    {
+        $loan = Loan::find($loanId);
+        if (!$loan) return response()->json(['error' => 'Loan not found']);
+
+        $loan->delete();
+        return response()->json(['message' => 'Loan deleted successfully']);
     }
 }
